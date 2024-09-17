ce8feebbbb4ea631c3630571be43173b4ad304cf
Igor <igorplan3@gmail.com>
2024-09-17 22:10:03 +0300

    slider crated

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -1,10 +1,36 @@
+import React, {useState} from 'react';
 import './Slider.css';
 import nextSlideArrow from './next.png';
 export default function Slider({ images }) {
+    const [indexSlide, setIndexSlider] = useState(0); 
+    const slides = 6;
+    const offsetToMove = (100 / 6).toFixed(2);
+    function onNextSlide (){
+        let indexCurrent = indexSlide + 1;
+        if(indexCurrent === slides) {
+            setIndexSlider(0)
+            return;
+        }
+        setIndexSlider(indexCurrent)
+    }
 
+    function onPrevSlide() {
+        let indexCurrent = indexSlide - 1;
+
+        if(indexCurrent < 0) {
+            setIndexSlider(slides - 1)
+            return;
+        }
+        setIndexSlider(indexCurrent)
+    
+    }
     return (
         <div className="car-slider slider">
-            <div className="slider-slides">
+            <div
+                style={ {
+                    'transform': `translateX(-${offsetToMove * indexSlide}%)`
+                } }
+                className="slider-slides">
                 { images.map((img) => {
                     return (
                         <div className="image-wrapper">
@@ -14,10 +40,15 @@ export default function Slider({ images }) {
                 }) }
             </div>
             <div className="slider__action-container">
-                <div className='slider__prev-slider'>
+                <div
+                    onClick={onPrevSlide}
+                    className='slider__prev-slider'>
                     <img src={nextSlideArrow} alt="arrow prev" />
                 </div>
-                <div className='slider__next-slider'>
+                <div
+                    onClick={onNextSlide}    
+                    className='slider__next-slider'
+                >
                     <img src={nextSlideArrow} alt="arrow prev" />
                 </div>
             </div>
